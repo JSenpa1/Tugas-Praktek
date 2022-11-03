93297548e219668c1d9d78c228ab5457b6f5ce42
unknown <jonathansusanto2015@gmail.com>
2022-11-03 20:44:36 +0700

    finished soal 5

---
+++ soal5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+ 
+int main()
+{
+    float a, PI;
+    PI = 3.14;
+    printf("CALCULATE VOLUME OF SPHERE\n\nRadius (meter) ? ");
+    scanf("%f", &a);
+    printf("Volume = %f", (4.0/3.0)*PI*(a*a*a));
+    getch();
+    return 0;
+}   
