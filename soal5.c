9d031838ee4fc60c4fd350b99f0951fafbc2ffae
Jojojo susan <jonathansusanto2015@gmail.com>
2022-11-09 10:28:04 +0700

    fixed soal 5

--- soal5.c
+++ soal5.c
@@ -8,7 +8,7 @@ int main()
     PI = 3.14;
     printf("CALCULATE VOLUME OF SPHERE\n\nRadius (meter) ? ");
     scanf("%f", &a);
-    printf("Volume = %f", (4.0/3.0)*PI*(a*a*a));
+    printf("Volume = %.3f", (4.0/3.0)*PI*(a*a*a));
     getch();
     return 0;
 }   
